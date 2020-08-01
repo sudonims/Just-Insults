d0554b9a6cca4368a010a4576ba759853de97674
Nimish Shah <nimishshah2000@gmail.com>
2020-08-01 23:47:52 +0530

    add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+### Instructions
+    1) Copy the code
+    2) Append it to .bashrc or /etc/bash.bashrc
+    3) Enjoy the insults
--- insult.sh
+++ insult.sh
@@ -1,15 +1,14 @@
-
-
-
-
-
 function command_not_found_handle () {
     insults=( 
         "Wow !! another wrong command... just rm -rf yourself dude"
         "Mr. Potato-head here...@${USER}"
         "Breins!!!"
-        "'${1}'...Really ?" 
+        "'${1}'...Really ?"
+        "You lower the IQ of whole street"
+        "Damn! You are a pain in the ass you know!"
+        "Use Windows, you dumb person"
+        "Disappointed but not surprised" 
         )
     
-    echo ${insults[$(( ( RANDOM % 3 ) ))]}
+    echo ${insults[$(( ( RANDOM % 8 ) ))]}
 }
